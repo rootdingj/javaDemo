6e6b5005cbd300940b3a3c155ca44e3034e989c2
steven <dj673344909@163.com>
2018-08-20 21:09:09 +0800

    比赛程序

---
+++ corejava/src/com/concurrency/ch01/User.java
@@ -0,0 +1,36 @@
+package com.yanxintec.competition;
+
+public class User implements Comparable<User>{
+	private String userId;
+	private int score;
+	public User() {
+		super();
+	}
+	public User(String userId, int score) {
+		super();
+		this.userId = userId;
+		this.score = score;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", score=" + score + "]";
+	}
+	
+	@Override
+	public int compareTo(User o) {
+		return this.getScore()-o.getScore();
+	}
+	
+}
