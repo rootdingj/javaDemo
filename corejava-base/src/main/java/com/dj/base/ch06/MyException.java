44dd274d67cd89d0d2c79375924dff9d8cb154f0
abcparadise <dj673344908@163.com>
2019-06-22 11:54:47 +0800

    add

---
+++ corejava-base/src/main/java/com/dj/base/ch06/MyException.java
@@ -0,0 +1,25 @@
+package com.dj.base.ch06;
+
+public class MyException extends Exception {
+
+	public MyException() {
+		super();
+	}
+
+	public MyException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+	}
+
+	public MyException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+	public MyException(String arg0) {
+		super(arg0);
+	}
+
+	public MyException(Throwable arg0) {
+		super(arg0);
+	}
+
+}
