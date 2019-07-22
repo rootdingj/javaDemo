724006b5050c12c0c4a080809dad758f37f05094
IamDingj <iamdingj@163.com>
2019-07-22 20:43:08 +0800

    add

---
+++ corejava-exam/src/main/java/com/dj/exam/ant/ch01/MyThread.java
@@ -0,0 +1,22 @@
+package com.dj.exam.ant.ch01;
+
+public class MyThread extends Thread{
+	
+	@Override
+	public void run() {
+		System.out.println(Singleton.getInstance().hashCode());
+	}
+	
+	public static void main(String[] args) {
+		MyThread[] mts = new MyThread[10];
+		for(int i = 0 ; i < mts.length ; i++){
+			mts[i] = new MyThread();
+		}
+		for (int j = 0; j < mts.length; j++) {
+			mts[j].start();
+		}
+	}
+	
+	
+
+}
