a4c8e75714e7b6bf3252e8186d086f7f909c9a35
steven_ting <steven_ting@kindee.com>
2023-09-11 14:30:12 +0800

    update

--- corejava-jvmjuc/src/main/java/com/dj/jvm/classLoader/TestClient.java
+++ corejava-jvmjuc/src/main/java/com/dj/jvm/classLoader/TestClient.java
@@ -4,7 +4,7 @@
 
     public static void main(String[] args) throws Exception {
         CustomClassLoader loader = new CustomClassLoader();
-        loader.setBasePath("D:\\resource\\git-steven\\corejava\\corejava-jvm\\target\\classes\\");
+        loader.setBasePath("D:\\project\\javaDemo\\corejava-jvmjuc\\src\\main\\java\\com\\dj\\jvm\\classLoader\\");
         Class<?> userClass = loader.findClass("com.dj.corejava.jvm.classLoader.User");
         System.out.println(userClass.getClassLoader());
         Object instance = userClass.newInstance();
